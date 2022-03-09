16eb470353e223c21af5191f3937730f354a7634
masaa0802 <masaakigoto0802@gmail.com>
2022-03-09 19:38:46 +0900

    PHP応用MVC作成事前準備完成

---
+++ PHP応用_演習/datebase.php
@@ -0,0 +1,30 @@
+<?php
+
+try {
+	// DBへ接続
+  $dbh = new PDO("mysql:host=127.0.0.1;port=3306;dbname = casteria'; charset=utf8");
+	$user = 'root';
+	$password = '';
+	// SQL作成
+	$sql = 'CREATE TABLE contacts (
+		id INT(11) AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(50) NOT NULL, 
+		kana VARCHAR(50) NOT NULL,
+    tel VARCHAR(11), 
+    email VARCHAR(100) NOT NULL,
+    body text,
+    created_at datetime CURRENT_TIMESTANP,
+		registry_datetime DATETIME,
+	) engine=innodb default charset=utf8';
+
+	// SQL実行
+	$res = $dbh->query($sql);
+
+} catch(PDOException $e) {
+
+	echo $e->getMessage();
+	die();
+}
+
+// 接続を閉じる
+$dbh = null;
