963352829ebc16325bfc42a5001d533f8fe92c18
masaa0802 <masaakigoto0802@gmail.com>
2022-03-25 14:46:59 +0900

    修正完了

--- PHP応用_演習/Models/ContactModel.php
+++ PHP応用_演習/Models/ContactModel.php
@@ -1,187 +1,182 @@
-<?php  
+<?php
+namespace Models\ContactModel;
 
-class Model {
-  private $pdo;
-  private static $dsn = 'mysql:charset=UTF8;dbname=casteria;host=127.0.0.1;port=8111';
-  private static $username = 'root';
-  private static $password = '';
+use \PDO;
+use \PDOException;
 
-  public $id;
-  public $name;
-  public $kana;
-  public $tel;
-  public $email;
-  public $body;
+class Model
+{
+    private $pdo;
+    private static $dsn = 'mysql:charset=UTF8;dbname=casteria;host=127.0.0.1;port=8111';
+    private static $username = 'root';
+    private static $password = '';
+    public $id;
+    public $name;
+    public $kana;
+    public $tel;
+    public $email;
+    public $body;
 
-  public function __construct(){
-
-      $this->pdo = new PDO(self::$dsn, self::$username, self::$password);
-      $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  }
-
-  public function creteData(){
-    try {
-      // トランザクション開始
-      $this->pdo->beginTransaction();
-      // SQL作成
-      $stmt = $this->pdo->prepare("SELECT * FROM contacts ORDER BY created_at ASC");
-      // SQL実行
-      $res = $stmt->execute();
-      return $stmt->fetchAll(PDO::FETCH_OBJ);
-      // コミット
-      if ($res) {
-          $this->pdo->commit();
-      }
-    } catch (PDOException $e) {
-      // エラーメッセージを出力
-      echo $e->getMessage();
-      // ロールバック
-      $this->pdo->rollBack();
-    } finally {
-      // データベースの接続解除
-      $this->pdo = null;
+    public function __construct()
+    {
+        $this->pdo = new PDO(self::$dsn, self::$username, self::$password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
-  }
 
-  public function insertData($name, $kana, $tel, $email, $body){       
-    try {
-        // トランザクション開始
-        $this->pdo->beginTransaction();
-        // SQL作成
-        $stmt = $this->pdo->prepare("INSERT INTO contacts (name, kana, tel, email, body) VALUES (:name, :kana, :tel, :email, :body)");
-  
-        // 値をセット
-        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-        $stmt->bindValue(':kana', $kana, PDO::PARAM_STR);
-        $stmt->bindValue(':tel', $tel, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-        $stmt->bindValue(':body', $body, PDO::PARAM_STR);
-  
-        // SQLクエリの実行
-        $res = $stmt->execute();
-  
-        // コミット
-        if ($res) {
-         $this->pdo->commit();
+    public function creteData()
+    {
+        try {
+            // トランザクション開始
+            $this->pdo->beginTransaction();
+            // SQL作成
+            $stmt = $this->pdo->prepare("SELECT * FROM contacts ORDER BY created_at ASC");
+            // SQL実行
+            $res = $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+            // コミット
+            if ($res) {
+                $this->pdo->commit();
+            }
+        } catch (PDOException $e) {
+            // エラーメッセージを出力
+            echo $e->getMessage();
+            // ロールバック
+            $this->pdo->rollBack();
+        } finally {
+            // データベースの接続解除
+            $this->pdo = null;
         }
-  
-    } catch(PDOException $e) {
-  
-        // エラーが発生した時はロールバック
-        $this->pdo->rollBack();
-        echo $e;
     }
-  
-    // プリペアドステートメントを削除
-    $stmt = null;
-  }
 
-  public function updateData($id, $name, $kana, $tel, $email, $body){
-    try {
-        // トランザクション開始
-        $this->pdo->beginTransaction();
-        // SQL作成
-        $stmt = $this->pdo->prepare("UPDATE contacts SET name = :name, kana = :kana, tel = :tel, email = :email , body = :body WHERE id = :id");
-
-        // 値をセット
-        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-        $stmt->bindValue(':kana', $kana, PDO::PARAM_STR);
-        $stmt->bindValue(':tel', $tel, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-        $stmt->bindValue(':body', $body, PDO::PARAM_STR);
-
-        // SQLクエリの実行
-        $res = $stmt->execute();
-
-        // コミット
-        if ($res) {
-          $this->pdo->commit();
+    public function insertData($name, $kana, $tel, $email, $body)
+    {
+        try {
+            // トランザクション開始
+            $this->pdo->beginTransaction();
+            // SQL作成
+            $stmt = $this->pdo->prepare("INSERT INTO contacts (name, kana, tel, email, body) 
+            VALUES (:name, :kana, :tel, :email, :body)");
+    
+            // 値をセット
+            $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmt->bindValue(':kana', $kana, PDO::PARAM_STR);
+            $stmt->bindValue(':tel', $tel, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+            $stmt->bindValue(':body', $body, PDO::PARAM_STR);
+    
+            // SQLクエリの実行
+            $res = $stmt->execute();
+    
+            // コミット
+            if ($res) {
+                $this->pdo->commit();
+            }
+        } catch (PDOException $e) {
+            // エラーが発生した時はロールバック
+            $this->pdo->rollBack();
+            echo $e;
         }
+    
+        // プリペアドステートメントを削除
+        $stmt = null;
+    }
 
-    } catch(PDOException $e) {
-        // エラーメッセージを出力
-        echo $e->getMessage();
-        // エラーが発生した時はロールバック
-        $this->pdo->rollBack();
-        echo $e;
+    public function updateData($id, $name, $kana, $tel, $email, $body)
+    {
+        try {
+            // トランザクション開始
+            $this->pdo->beginTransaction();
+            // SQL作成
+            $stmt = $this->pdo->prepare("UPDATE contacts 
+            SET name = :name, kana = :kana, tel = :tel, email = :email , body = :body WHERE id = :id");
+
+            // 値をセット
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmt->bindValue(':kana', $kana, PDO::PARAM_STR);
+            $stmt->bindValue(':tel', $tel, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+            $stmt->bindValue(':body', $body, PDO::PARAM_STR);
+
+            // SQLクエリの実行
+            $res = $stmt->execute();
+
+            // コミット
+            if ($res) {
+                $this->pdo->commit();
+            }
+        } catch (PDOException $e) {
+            // エラーメッセージを出力
+            echo $e->getMessage();
+            // エラーが発生した時はロールバック
+            $this->pdo->rollBack();
+            echo $e;
+        }
+        // 更新に成功したら一覧に戻る
+        if ($res) {
+            header("Location: ./contact.php");
+            exit;
+        }
+        // プリペアドステートメントを削除
+        $stmt = null;
     }
-    // 更新に成功したら一覧に戻る
-    if ($res) {
-      header("Location: ./contact.php");
-      exit;
-    }
-    // プリペアドステートメントを削除
-    $stmt = null;
-  }
   
 
-  public function editData($id){  
+    public function editData($id)
+    {
+        try {
+            // トランザクション開始
+            $this->pdo->beginTransaction();
+            // SQL作成
+            $stmt = $this->pdo->prepare("SELECT * FROM contacts WHERE id = :id");
 
-    try{
-      // トランザクション開始
-      $this->pdo->beginTransaction();
-      // SQL作成
-      $stmt = $this->pdo->prepare("SELECT * FROM contacts WHERE id = :id");
+            // 値をセット
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 
-      // 値をセット
-      $stmt->bindValue( ':id', $id, PDO::PARAM_INT);
-
-      // SQLクエリの実行
-      $stmt->execute();
-    } catch (PDOException $e) {
-      // エラーメッセージを出力
-      echo $e->getMessage();
-      // ロールバック
-      $this->pdo->rollBack();
-    } finally {
-        // データベースの接続解除
-        $this->pdo = null;
+            // SQLクエリの実行
+            $stmt->execute();
+        } catch (PDOException $e) {
+            // エラーメッセージを出力
+            echo $e->getMessage();
+            // ロールバック
+            $this->pdo->rollBack();
+        } finally {
+            // データベースの接続解除
+            $this->pdo = null;
+        }
+        // 表示するデータを取得
+        return $stmt->fetch();
     }
-    // 表示するデータを取得
-    return $stmt->fetch();
-  }
 
-  public function deleteData($id){
+    public function deleteData($id)
+    {
 
-    try {
-      // トランザクション開始
-      $this->pdo->beginTransaction();
-      // SQL作成
-      $stmt = $this->pdo->prepare("DELETE FROM contacts WHERE id = :id");
+        try {
+            // トランザクション開始
+            $this->pdo->beginTransaction();
+            // SQL作成
+            $stmt = $this->pdo->prepare("DELETE FROM contacts WHERE id = :id");
+            // 値をセット
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 
-      // 値をセット
-      $stmt->bindValue(':id',$id, PDO::PARAM_INT);
+            // SQLクエリの実行
+            $res = $stmt->execute();
 
-      // SQLクエリの実行
-      $res = $stmt->execute();
-
-      // コミット
-      if ($res) {
-         $this->pdo->commit();
-      }
-
-    } catch(PDOException $e) {
-
-      // エラーが発生した時はロールバック
-      $this->pdo->rollBack();
-      echo $e;
-    }
+            // コミット
+            if ($res) {
+                $this->pdo->commit();
+            }
+        } catch (PDOException $e) {
+            // エラーが発生した時はロールバック
+            $this->pdo->rollBack();
+            echo $e;
+        }
 
     // 削除に成功したら一覧に戻る
-    if ($res) {
-      header("Location: contact.php");
-      exit;
+        if ($res) {
+            header("Location: contact.php");
+            exit;
+        }
     }
-  }
 }
-
-
-
-
-
-
-
-
-
--- PHP応用_演習/Views/edit.php
+++ PHP応用_演習/Views/edit.php
@@ -1,10 +1,10 @@
-<?php 
-require_once(ROOT_PATH .'Controllers/ContactController.php'); 
-if (!empty($_POST)){
-  $id = $_POST['id'];
-  if($id){
-  $errors = $controller->validation();
-  }
+<?php
+require_once(ROOT_PATH .'Controllers/ContactController.php');
+if (!empty($_POST)) {
+    $id = $_POST['id'];
+    if ($id) {
+        $errors = $controller->validation();
+    }
 }
 ?>
 
@@ -19,15 +19,15 @@ if (!empty($_POST)){
   <h1> 詳細画面 </h1>
     <p>* は必須項目です</p> 
 
-      <?php if(!empty($errors)):?>
-        <ul class="error_list">
-        <?php foreach($errors as $value):?>
-          <li><?php echo $value;?></li>
-        <?php endforeach; ?>
-        </ul>
-      <?php endif; ?>
-      <form action= "" method="post">
-      <?php
+        <?php if (!empty($errors)) :?>
+            <ul class="error_list">
+            <?php foreach ($errors as $value) :?>
+                <li><?php echo $value;?></li>
+            <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+        <form action= "" method="post">
+        <?php
         // GETパラメータの$idがあり、POSTデータがない場合、データを表示する
         if (!empty($_GET['id'])&& empty($_POST['id'])) {
             $message_data = $controller->editData($_GET['id']);
@@ -42,56 +42,77 @@ if (!empty($_POST)){
             $body = $_POST['body'];
             // DBを更新する
             $controller->updateData($id, $name, $kana, $tel, $email, $body);
-        }// GETパラメータの$idが空で、POSTデータがない場合、contact.phpにリダイレクト
-          // 不正アクセスの防止
-          elseif (empty($_GET['id'] && empty($_POST))) {
+        // GETパラメータの$idが空で、POSTデータがない場合、contact.phpにリダイレクト
+        // 不正アクセスの防止
+        } elseif (empty($_GET['id'] && empty($_POST))) {
             header("Location: contact.php");
             exit;
-        } 
-      ?>
+        }
+        ?>
         <div class="row">
           <!-- 氏名  -->
           <div class="element_wrap">
             <label for="name">*氏名</label>
-            <input type="text" name="name" id="name" placeholder="例）入力太郎"
-            value="<?php if (!empty($message_data['name'])){ echo $message_data['name'];} 
-            elseif (!empty($name)){ echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8');}
-            ?>"><br>
+            <input type="text" name="name" id="name" placeholder="例）入力太郎" value="
+            <?php
+            if (!empty($message_data['name'])) {
+                echo $message_data['name'];
+            } elseif (!empty($name)) {
+                echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+            } ?>"><br>
           </div>
           <!-- フリガナ -->
           <div class="element_wrap">
             <label for="kana">*フリガナ</label>
-            <input type="text" name="kana" id="kana" placeholder="例）ニュウリョクタロウ"
-            value="<?php if ( !empty($message_data['kana'])){ echo $message_data['kana'];}
-            elseif ( !empty($kana)){ echo htmlspecialchars( $kana, ENT_QUOTES, 'UTF-8');}
-            ?>"><br>
+            <input type="text" name="kana" id="kana" placeholder="例）ニュウリョクタロウ" value="
+            <?php
+            if (!empty($message_data['kana'])) {
+                echo $message_data['kana'];
+            } elseif (!empty($kana)) {
+                echo htmlspecialchars($kana, ENT_QUOTES, 'UTF-8');
+            } ?>"><br>
           </div>
           <!-- 電話番号 -->
           <div class="element_wrap">
             <label for="tel">電話番号</label>
-            <input type="text" name="tel" id="tel" placeholder="例）08011112222"
-            value="<?php if ( !empty($message_data['tel'])){ echo $message_data['tel'];}
-            elseif( !empty($tel)){ echo htmlspecialchars( $tel, ENT_QUOTES, 'UTF-8');}
-            ?>"><br>
+            <input type="text" name="tel" id="tel" placeholder="例）08011112222" value="
+            <?php
+            if (!empty($message_data['tel'])) {
+                echo $message_data['tel'];
+            } elseif (!empty($tel)) {
+                echo htmlspecialchars($tel, ENT_QUOTES, 'UTF-8');
+            } ?>"><br>
           </div>
           <!-- メールアドレス -->
           <div class="element_wrap">
             <label for="email">*メールアドレス</label>
-            <input type="text" name="email" id="email" placeholder="例）nyuuryoku11@mail.com"
-            value="<?php if ( !empty($message_data['email'])){ echo $message_data['email'];} 
-            elseif( !empty($email)){ echo htmlspecialchars( $email, ENT_QUOTES, 'UTF-8');}
-            ?>"><br>
+            <input type="text" name="email" id="email" placeholder="例）nyuuryoku11@mail.com" value="
+            <?php
+            if (!empty($message_data['email'])) {
+                echo $message_data['email'];
+            } elseif (!empty($email)) {
+                echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+            }?>"><br>
           </div>
           <!-- お問い合わせ内容 -->
           <div class="element_wrap">
             <label for="body">*お問い合わせ内容</label><br>
-            <textarea rows="10" name="body" id="body" ><?php if (!empty($message_data['body'])){ echo nl2br($message_data['body']);} 
-            elseif( !empty($body)){ echo htmlspecialchars( $body, ENT_QUOTES, 'UTF-8');}
-            ?></textarea><br>
+            <textarea rows="10" name="body" id="body" >
+                <?php
+                if (!empty($message_data['body'])) {
+                    echo nl2br($message_data['body']);
+                } elseif (!empty($body)) {
+                    echo htmlspecialchars($body, ENT_QUOTES, 'UTF-8');
+                }?>
+            </textarea><br>
           </div>
           <a class="btn_cancel" href="contact.php">キャンセル</a>
           <input name = "btn_update" type="submit" value="更新">
-          <input type="hidden" name="id" value="<?php if( !empty($message_data['id'])){ echo $message_data['id'];} ?>">
+          <input type="hidden" name="id" value="
+            <?php
+            if (!empty($message_data['id'])) {
+                echo $message_data['id'];
+            } ?>">
         </div>
       </form>
   </body>
